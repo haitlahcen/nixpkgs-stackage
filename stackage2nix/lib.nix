aa8befdcc6481d6e822c2db5198ee83e25f7a200
Hussein Ait-Lahcen <hussein.ait-lahcen@fretlink.com>
2018-11-06 22:44:03 +0100

    fix impure derivations

--- stackage2nix/lib.nix
+++ stackage2nix/lib.nix
@@ -7,10 +7,16 @@
     name = "hackage-db";
     version = cacheVersion;
     phases = [ "installPhase" ];
-    src = builtins.fetchTarball https://hackage.haskell.org/01-index.tar.gz;
+    src = builtins.fetchurl {
+      url = "https://hackage.haskell.org/01-index.tar.gz";
+      sha256 = "0hczs7fldxliw413ky926f4gp0c5dsxrxpy9kmm9v2fdh0gs9y81";
+    };
     installPhase = ''
       mkdir -p $out
-      cp -r $src $out
+      pushd $out
+      cp $src 01-index.tar.gz
+      gunzip --keep 01-index.tar.gz
+      popd
     '';
     SSL_CERT_FILE="${cacert}/etc/ssl/certs/ca-bundle.crt";
   };
@@ -20,8 +26,9 @@
     version = cacheVersion;
     phases = [ "installPhase" ];
     src = builtins.fetchGit {
-      url = https://github.com/commercialhaskell/all-cabal-hashes.git;
+      url = "https://github.com/commercialhaskell/all-cabal-hashes.git";
       ref = "hackage";
+      rev = "abed6aa79c87ba374580615dd776863531d301ab";
     };
     installPhase = ''
       mkdir -p $out
@@ -35,8 +42,9 @@
     version = cacheVersion;
     phases = [ "installPhase" ];
     src = builtins.fetchGit {
-      url = https://github.com/fpco/lts-haskell.git;
+      url = "https://github.com/fpco/lts-haskell.git";
       ref = "master";
+      rev = "3edcfa5096acd1ef88e35bc00d54164f154237a1";
     };
     installPhase = ''
       mkdir -p $out
