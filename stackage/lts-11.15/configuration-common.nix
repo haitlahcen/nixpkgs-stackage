8f40b6d6f6a1eaa83643bda26940838f3ba280fc
Hussein Ait-Lahcen <hussein.ait-lahcen@fretlink.com>
2018-11-08 23:30:37 +0100

    disable profiling & haddock

--- stackage/lts-11.15/configuration-common.nix
+++ stackage/lts-11.15/configuration-common.nix
@@ -1,8 +1,6 @@
 { pkgs, haskellLib }:
 
 with haskellLib; self: super: {
-  cabal2nix = dontCheck super.cabal2nix;
-  hlibgit2 = pkgs.haskellPackages.hlibgit2;
   mkDerivation = args: super.mkDerivation (args // {
     enableLibraryProfiling = false;
     enableExecutableProfiling = false;
